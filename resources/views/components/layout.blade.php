ffc68e3e584ba4ed9d30809b71f37c5669387ba2
Victor Oliveira <victor.oliveira@blueservice.com.br>
2023-01-20 15:16:37 -0300

    controle de series laravel .

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }} - Controle de séries</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>{{ $title }}</h1>
+
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
